29bcdb36ab4d00c1b0b3915c92e44d9db299e2a6
Martin <mcekodhima@gmail.com>
2016-12-31 12:44:23 +0100

    Uplaod for Heroku

--- gallery/show/api.py
+++ gallery/show/api.py
@@ -1,22 +1,25 @@
 from .models import Image
-from .serializers import ImageSerializer
+from .serializers import ImageSerializer, ImageDetailSerializer
 from django.http import Http404
 import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import base64
+from django.core.files.base import ContentFile
 
 class PhotoList(APIView):
+            
     def get(self, request):
         images = Image.objects.all()
         serializedImages = ImageSerializer(images, many=True)
         return Response(serializedImages.data)
     
-    def post(self, request):
-        serializer = ImageSerializer(data=request.data,files=request.FILES)
+    def put(self,request):
+        serializer = ImageDetailSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class PhotoDetail(APIView):
@@ -28,26 +31,18 @@ class PhotoDetail(APIView):
         
     def get(self, request, pk):
         images = self.get_objects(pk)
-        serializedImages = ImageSerializer(images)
+        serializedImages = ImageDetailSerializer(images)
         return Response(serializedImages.data)
         
-    def delete(self,request,pk):
+    def delete(self, request, pk):
         images = self.get_objects(pk)
         images.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-    def put(self,request,pk):
+    def post(self, request, pk):
         images = self.get_objects(pk)
-        serializer = ImageSerializer(images, data=request.data, files=request.files)
+        serializer = ImageSerializer(images, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def post(self,request,pk):
-        images = self.get_objects(pk)
-        serializer = ImageSerializer(images, data=request.data, files=request.files)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- gallery/show/serializers.py
+++ gallery/show/serializers.py
@@ -1,7 +1,15 @@
 from .models import Image
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
-class ImageSerializer(serializers.ModelSerializer):
+class ImageSerializer(serializers.ModelSerializer):   
+    photo = Base64ImageField(required=True)
+    class Meta:
+        model = Image
+        fields = ['id', 'title', 'keyword1', 'keyword2', 'keyword3', 'photo']
+
+class ImageDetailSerializer(serializers.ModelSerializer):   
+    photo = Base64ImageField(required=True)
     class Meta:
         model = Image
         fields = ['id', 'title', 'user', 'keyword1', 'keyword2', 'keyword3', 'timestamp', 'photo']
